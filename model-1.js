667229a7d02bd0b95a007cc2dc82c230011e6b70
Stevie Ambroise <s.ambroise@protonmail.com>
2021-08-30 20:59:24 -0600

    Show variables that are strings

---
+++ model-1.js
@@ -0,0 +1,18 @@
+//Real world thing of my choice is family dinner
+
+//strings
+var favoriteDish = "lasagna"
+var lasagna = "yum yum in tum tum"
+var howWeFeel = "Happy to be here!"
+
+
+//integers
+
+
+//boolean
+
+
+//arrays
+
+
+//objects
